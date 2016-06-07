6a5762d6bb732c8a0401eaf83be6c8de569bbf65
Benny Degezelle <hi@monkeypatch.be>
2016-06-08 00:14:50 +0200

    Add support for 'ids' attribute to r:aggregate

--- lib/find_by_id/tag_extension.rb
+++ lib/find_by_id/tag_extension.rb
@@ -1,28 +1,50 @@
 module FindById::TagExtension
   include Radiant::Taggable
   
-  
-    desc %{
-      Inside this tag all page related tags refer to the page found at the @path@ attribute.
-      @path@s may be relative or absolute paths.
-      If an @id@ attribute is passed, the path attribute will be ignored
-      *Usage:*
+  desc %{
+    Inside this tag all page related tags refer to the page found at the @path@ attribute.
+    @path@s may be relative or absolute paths.
+    If an @id@ attribute is passed, the path attribute will be ignored
+    *Usage:*
 
-      <pre><code><r:find path="value_to_find">...</r:find></code></pre>
-    }
-    tag 'find' do |tag|
-      required_attr(tag,'path','url','id')
+    <pre><code><r:find path="value_to_find">...</r:find></code></pre>
+  }
+  tag 'find' do |tag|
+    required_attr(tag,'path','url','id')
 
-      if id = tag.attr.delete('id')
-        found = Page.find(id.to_i)
-      else
-        path = tag.attr['path'] || tag.attr['url']
-        found = Page.find_by_path(absolute_path_for(tag.locals.page.path, path))
-      end
-      if page_found?(found)
-        tag.locals.page = found
-        tag.expand
-      end
+    if id = tag.attr.delete('id')
+      found = Page.find(id.to_i)
+    else
+      path = tag.attr['path'] || tag.attr['url']
+      found = Page.find_by_path(absolute_path_for(tag.locals.page.path, path))
     end
+    if page_found?(found)
+      tag.locals.page = found
+      tag.expand
+    end
+  end
+
+  desc %{
+    Aggregates the children of multiple paths using the @paths@ or @ids@ attribute.
+    Useful for combining many different sections/categories into a single
+    feed or listing.
+    
+    *Usage*:
+    
+    <pre><code><r:aggregate paths="/section1; /section2; /section3"> ... </r:aggregate>
+    <r:aggregate ids="4; 6; 7"> ... </r:aggregate></code></pre>
+  }
+  tag "aggregate" do |tag|
+    required_attr(tag, 'paths', 'urls', 'ids')
+    if ids = tag.attr.delete('ids')
+      tag.locals.parent_ids = ids.split(';').map(&:strip).reject(&:blank?)
+    else
+      paths = (tag.attr['paths']||tag.attr["urls"]).split(";").map(&:strip).reject(&:blank?).map { |u| clean_path u }
+      parent_ids = paths.map {|u| Page.find_by_path(u) }.map(&:id)
+      tag.locals.parent_ids = parent_ids
+    end
+    tag.expand
+  end
+  
 
 end
