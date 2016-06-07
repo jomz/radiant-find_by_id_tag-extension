6298902af5071ddf6a56be6b0fb0125361d48621
Benny Degezelle <hi@monkeypatch.be>
2016-06-08 01:15:54 +0200

    Update gem info

--- lib/radiant-find_by_id_tag-extension.rb
+++ lib/radiant-find_by_id_tag-extension.rb
@@ -1,7 +1,7 @@
 module RadiantFindByIdTagExtension
-  VERSION     = "1.0.0"
+  VERSION     = "1.1.0"
   SUMMARY     = "Find By Id Tag for Radiant CMS"
-  DESCRIPTION = "Adds id attribute to r:find"
+  DESCRIPTION = "Adds support for an 'id' attribute to r:find"
   URL         = "http://github.com/jomz/radiant-find_by_id_tag-extension"
   AUTHORS     = ["Benny Degezelle"]
   EMAIL       = ["hi@monkeypatch.be"]
