37a401cee7b54ad6cd91a2f644b5c61786384a66
Benny Degezelle <hi@monkeypatch.be>
2016-06-10 18:01:07 +0200

    Add partials to display page ids on overview and detail admin pages

--- find_by_id_tag_extension.rb
+++ find_by_id_tag_extension.rb
@@ -7,5 +7,10 @@ class FindByIdTagExtension < Radiant::Extension
 
   def activate
     Page.send :include, FindById::TagExtension
+    
+    admin.pages.index.add :sitemap_head, "id_column_th", :before => "title_column_header"
+    admin.pages.index.add :node, "id_column_td", :before => "title_column"
+    admin.pages.edit.add :main, "id_field", :before => "edit_form"
+    
   end
 end
